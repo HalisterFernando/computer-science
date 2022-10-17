2db2611a43347859b4488f7bca44511b81b28735
Seu nome <halisterfernando@hotmail.com>
2022-10-17 19:59:22 -0300

    chore: :rocket: Crie uma função que receba dois números e retorne o maior deles

---
+++ DIA_1_APRENDENDO_PYTHON/dia_1.py
@@ -0,0 +1,11 @@
+# 🚀 Exercício 1: Crie uma função que receba dois números
+# e retorne o maior deles.
+
+
+def greatest_number(num1, num2):
+    numbers = [num1, num2]
+    return max(numbers)
+
+
+print(greatest_number(50, 100))
+print(greatest_number(500, 100))
