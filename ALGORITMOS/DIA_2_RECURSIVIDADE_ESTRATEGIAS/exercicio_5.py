c1b0ce7a496f1d91afab193bb75562b1237450a6
Seu nome <halisterfernando@hotmail.com>
2022-11-23 17:40:00 -0300

    chore: :rocket: recursividade

---
+++ ALGORITMOS/DIA_2_RECURSIVIDADE_ESTRATEGIAS/exercicio_5.py
@@ -0,0 +1,14 @@
+# def prime_number(n):
+#     count = 0
+#     if n == 1 or n == 2:
+#         return "fim"
+#     elif n % n - 1 == 0:
+#         count += 1
+#         print(count)
+#         return prime_number(n - 1)
+#     else:
+#         return prime_number(n - 1)
+
+
+# if __name__ == "__main__":
+#     print(prime_number(7))
